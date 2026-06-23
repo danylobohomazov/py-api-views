5d32914eb0d35350235774711557e595c5288372
danil <danil.bogomazov1356@gmail.com>
2026-06-23 07:53:50 +0300

    first try

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    genres = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    actors = models.ManyToManyField(Actor)
     duration = models.IntegerField()
 
     def __str__(self):
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,49 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallList,
+    CinemaHallDetail,
+    MovieViewSet
+)
+
+movie_list = MovieViewSet.as_view(actions={"get": "list", "post": "create"})
+movie_detail = MovieViewSet.as_view(
+            actions={
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy"
+            }
+        )
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genre_list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genre_detail"),
+    path("actors/", ActorList.as_view(), name="actor_list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
+    path(
+        "cinema_halls/",
+        CinemaHallList.as_view(actions={"get": "list", "post": "create"}),
+        name="cinema_hall_list",
+    ),
+    path(
+        "cinema_halls/<int:pk>/",
+        CinemaHallDetail.as_view(
+            actions={
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy"
+            }
+        ),
+        name="cinema_hall_detail"
+    ),
+    path("movies/", movie_list, name="movie_list"),
+    path("movies/<int:pk>/", movie_detail, name="movie_detail"),
 ]
 
 app_name = "cinema"
